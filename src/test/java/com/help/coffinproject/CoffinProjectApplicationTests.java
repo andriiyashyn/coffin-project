135489c67803b2b9e14e4246fb3e9ca83c7ebf85
Andrii Yashyn <andrii.yashyn@auto1.com>
2022-04-18 11:43:56 +0300

    init

---
+++ src/test/java/com/help/coffinproject/CoffinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.help.coffinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffinProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
